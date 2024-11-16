3f5e2451be59deab94b0a3e3b0eb55cbae284742
Vakkruz <jwest-williams@protonmail.com>
2024-11-16 12:06:30 -0500

    Write function takes input

--- server_report.ps1
+++ server_report.ps1
@@ -3,5 +3,4 @@ Import-Module .\svr_rpt_functions.psm1
 
 Initialize-ServerFile
 Get-ServerFile
-Write-ServerFile
 
--- svr_rpt_functions.psm1
+++ svr_rpt_functions.psm1
@@ -1,34 +1,17 @@
 #===== Functions for Report Script =======
 
-function Initialize-ServerFile { #Creates file to be used for writing report
-    $date = Get-Date -Format "yyyy.MM.dd" #All reports will have the date as the suffix to make unique, in case there are multiple old reports in directory
-    Out-File -FilePath .\ServerStatus-$date.txt
-}
 function Get-ServerFile { #Retrieves filename of report
     $date = Get-Date -Format "yyyy.MM.dd"
     Get-ChildItem -Path "./*$date.txt" -Name
 }
 
-function Write-ServerFile{
+function Write-ServerFile($output){ #Main function to write to the txt file. Takes one parameter: the thing to be written
     $filename = Get-ServerFile
-    Get-Date -Format "yyyy.MM.dd HH:mm:ss.ffff" | Out-File -FilePath .\$filename -Append 
-    Get-Date -Format "yyyy.MM.dd HH:mm:ss.ffff" | Out-File -FilePath .\$filename -Append 
-    Get-Date -Format "yyyy.MM.dd HH:mm:ss.ffff" | Out-File -FilePath .\$filename -Append 
-    Get-Date -Format "yyyy.MM.dd HH:mm:ss.ffff" | Out-File -FilePath .\$filename -Append 
-    Get-Date -Format "yyyy.MM.dd HH:mm:ss.ffff" | Out-File -FilePath .\$filename -Append 
-    Get-Date -Format "yyyy.MM.dd HH:mm:ss.ffff" | Out-File -FilePath .\$filename -Append 
-    Get-Date -Format "yyyy.MM.dd HH:mm:ss.ffff" | Out-File -FilePath .\$filename -Append 
-    Get-Date -Format "yyyy.MM.dd HH:mm:ss.ffff" | Out-File -FilePath .\$filename -Append 
-    Get-Date -Format "yyyy.MM.dd HH:mm:ss.ffff" | Out-File -FilePath .\$filename -Append 
-    Get-Date -Format "yyyy.MM.dd HH:mm:ss.ffff" | Out-File -FilePath .\$filename -Append 
-    Get-Date -Format "yyyy.MM.dd HH:mm:ss.ffff" | Out-File -FilePath .\$filename -Append 
-    Get-Date -Format "yyyy.MM.dd HH:mm:ss.ffff" | Out-File -FilePath .\$filename -Append 
-    Get-Date -Format "yyyy.MM.dd HH:mm:ss.ffff" | Out-File -FilePath .\$filename -Append 
+    $output | Out-File -FilePath .\$filename -Append 
+}
 
-    Get-Date -Format "yyyy.MM.dd HH:mm:ss.ffff" | Out-File -FilePath .\$filename -Append 
-    Get-Date -Format "yyyy.MM.dd HH:mm:ss.ffff" | Out-File -FilePath .\$filename -Append 
-    Get-Date -Format "yyyy.MM.dd HH:mm:ss.ffff" | Out-File -FilePath .\$filename -Append 
-    Get-Date -Format "yyyy.MM.dd HH:mm:ss.ffff" | Out-File -FilePath .\$filename -Append 
-
-    Get-Date -Format "yyyy.MM.dd HH:mm:ss.ffff" | Out-File -FilePath .\$filename -Append 
+function Initialize-ServerFile { #Creates file to be used for writing report
+    $date = Get-Date -Format "yyyy.MM.dd" #All reports will have the date as the suffix to make unique, in case there are multiple old reports in directory
+    Out-File -FilePath .\ServerStatus-$date.txt
+    Write-ServerFile("Hello asshole")
 }
