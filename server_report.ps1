252f16de6e440ca6693e2a9bc2f52c1cdb38a9c5
Vakkruz <jwest-williams@protonmail.com>
2024-11-16 14:59:54 -0500

    Added in error catching; v1.0.0

--- server_report.ps1
+++ server_report.ps1
@@ -1,34 +1,38 @@
 Import-Module .\svr_rpt_functions.psm1
 
-
-Write-Host "////////////////////////////////////////////////"-ForegroundColor DarkRed -BackgroundColor Black
-Write-Host "        SERVER STATUS SCRIPT Initiated!" -ForegroundColor DarkRed -BackgroundColor Black
-Write-Host "////////////////////////////////////////////////"-ForegroundColor DarkRed -BackgroundColor Black
+Write-Host "////////////////////////////////////////////////"-ForegroundColor Magenta -BackgroundColor Black
+Write-Host "        SERVER STATUS SCRIPT Initiated!" -ForegroundColor Magenta -BackgroundColor Black
+Write-Host "////////////////////////////////////////////////"-ForegroundColor Magenta -BackgroundColor Black
 Write-Host ""
 
-Write-Host "////////////////////////////////////////////////"-ForegroundColor DarkRed -BackgroundColor Black
-Write-Host "Specify the hostnames of servers you'd like reports for"-ForegroundColor DarkRed -BackgroundColor Black
+Write-Host "////////////////////////////////////////////////"-ForegroundColor Magenta -BackgroundColor Black
+Write-Host "Specify the hostnames of servers you'd like reports for"-ForegroundColor Magenta -BackgroundColor Black
 Write-Host "	NOTE: For multiple groups, use commas with no leading spaces (Ex: Group A,Group B,Group D,...)"-ForegroundColor White -BackgroundColor Black
 $allServers = Read-Host "Enter here " 
 
 $splitServers = $allServers -split ',' #Splits the groups based on the commas so the for loop later can read it
 
 Write-Host $splitServers
-Write-Host "Does this look correct to you? Enter Y to proceed to account creation or N to quit" -ForegroundColor DarkRed -BackgroundColor Black
+Write-Host "Does this look correct to you? Enter Y to proceed to account creation or N to quit" -ForegroundColor Magenta -BackgroundColor Black
 $continue = Read-Host "Enter here"
 if($continue -eq "Y"){
 
     Initialize-ServerFile
 
     foreach ($Server in $splitServers){
-        Write-ServerFile("=============$Server=============")
-        Write-ServerFile("")
-        Write-Memory($Server)
-        Write-CPULoad($Server)
-        Write-Storage($Server)
+        if ($Server -eq ""){#If the server list has a blank entry, skip it to prevent a crash
+            Write-Host "=== BLANK ENTRY DETECTED. SKIPPING... ===" -ForegroundColor Red -BackgroundColor Black
+        }else{
+            Write-Host "PROCESSING $Server. Please wait..." -ForegroundColor Magenta -BackgroundColor Black
+            Write-ServerFile("=============$Server=============")
+            Write-ServerFile("")
+            Write-Memory($Server)
+            Write-CPULoad($Server)
+            Write-Storage($Server)
+        }
+
     }
+    Write-Host "Program Complete! Look for the filename below in the current directory!"
 }else{
     Write-Host "Program Terminated!"
 }
-
-Write-Host "Program Complete! Look for the filename below in the current directory!"
--- svr_rpt_functions.psm1
+++ svr_rpt_functions.psm1
@@ -1,5 +1,7 @@
 #===== Functions for Report Script =======
 
+$ErrorActionPreference = 'Stop' #Any error generated 'stops' the script, allowing the use of Try-Catch 
+
 function Get-TimeofDay{ #Generates date suffix to be used for regex in other functions
     Get-Date -Format "yyyy.MM.dd"
 }
@@ -21,7 +23,7 @@ function Initialize-ServerFile { #Creates file to be used for writing report
         $filename = Get-ServerFile
         $output | Out-File -FilePath .\$filename
         Write-ServerFile("SERVER STATUS REPORT--GENERATED $date")
-    }else{
+    }else{#Create the file if it doesn't exist
         $date = Get-TimeofDay 
         Out-File -FilePath .\ServerStatus-$date.txt
         Write-ServerFile("SERVER STATUS REPORT--GENERATED $date")
@@ -30,15 +32,38 @@ function Initialize-ServerFile { #Creates file to be used for writing report
 }
 
 function Write-Memory($hostname){
-    Write-ServerFile(Get-Counter -Counter "\memory\available mbytes" -computername $hostname)
-    Write-ServerFile("")
+    try{
+        Write-ServerFile(Get-Counter -Counter "\memory\available mbytes" -computername $hostname) 
+    }catch{
+        Write-Host ">>>> Powershell detected an error with Hostname: $hostname. Will not write to file. Check spelling or connect directly to server." -ForegroundColor Red -BackgroundColor Black
+    }finally{
+        Write-ServerFile("")
+    }
 }
 
 function Write-CPULoad($hostname){
-    Write-ServerFile(Get-WmiObject win32_processor -computername $hostname | Measure-Object -property LoadPercentage -Average | Select Average)
-    Write-ServerFile("")
+    try {
+        Write-ServerFile(Get-WmiObject win32_processor -computername $hostname | Measure-Object -property LoadPercentage -Average | Select Average)
+    }
+    catch {
+        Write-Host ">>>> Powershell detected an error with Hostname: $hostname. Will not write to file. Check spelling or connect directly to server." -ForegroundColor Red -BackgroundColor Black
+    }
+    finally {
+        Write-ServerFile("")
+    }
 }
 
 function Write-Storage($hostname){
-    Write-ServerFile(Get-WmiObject -Class Win32_LogicalDisk -ComputerName $hostname | ? {$_. DriveType -eq 3} | select DeviceID, {$_.Size /1GB}, {$_.FreeSpace /1GB})
+    try {
+        Write-ServerFile(Get-WmiObject -Class Win32_LogicalDisk -ComputerName $hostname | ? {$_. DriveType -eq 3} | select DeviceID, {$_.Size /1GB}, {$_.FreeSpace /1GB})
+    }
+    catch {
+        Write-Host ">>>> Powershell detected an error with Hostname: $hostname. Will not write to file. Check spelling or connect directly to server." -ForegroundColor Red -BackgroundColor Black
+    }
+    finally {
+        Write-ServerFile("")
+    }
+
+
+    
 }
